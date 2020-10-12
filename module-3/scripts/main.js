376d187ca71898ca7b87c4c7b02868ede21cc04e
Suzy Poole <72015518+SuzyPoole@users.noreply.github.com>
2020-10-12 11:27:21 +0100

    Create main.js

---
+++ module-3/scripts/main.js
@@ -0,0 +1,18 @@
+
+var numberOfSmarties = 40;
+
+var numberOfIceCreams = 30;
+
+var total = numberOfSmarties + numberOfIceCreams; // 40 + 30 =70
+
+console.log ( total ); // 70
+
+/*
++ add
+- subtract
+/ divide
+* multiply
+% modulus
+*/
+
+
