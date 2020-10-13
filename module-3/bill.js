a5e0fdbd767de052fbab10f3c603ca0a95926af3
Suzy Poole <72015518+SuzyPoole@users.noreply.github.com>
2020-10-13 09:56:51 +0100

    Create bill.js

---
+++ module-3/bill.js
@@ -0,0 +1,11 @@
+var charge = 25.00;
+var tip = charge * 10 / 100;
+var total = charge + tip;
+
+var receipt = 'The bill plus tip is ' +(charge + tip);
+
+/*var num = total;
+var n = num.toFixed(2);*/
+
+
+console.log( receipt );
